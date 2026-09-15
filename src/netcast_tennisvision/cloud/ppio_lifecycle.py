bf6d73fef4bfdac5b334e8da42521538c0dea325
AndyLongest <yuanzhlang@mail2.sysu.edu.cn>
2026-09-15 21:32:50 +0800

    Publish lightweight production image overlays

--- src/netcast_tennisvision/cloud/ppio_lifecycle.py
+++ src/netcast_tennisvision/cloud/ppio_lifecycle.py
@@ -48,7 +48,7 @@ class PPIOJobManager:
         self.shared_secret = os.environ.get("TENNISVISION_CLOUD_TOKEN", "").strip()
         self.image = os.environ.get(
             "TENNISVISION_PPIO_IMAGE",
-            "image.ppinfra.com/prod-ahskpcitxxwcgdnfqfpu/netcast-tennisvision:production-v2",
+            "image.ppinfra.com/prod-ahskpcitxxwcgdnfqfpu/netcast-tennisvision:production-v3",
         ).strip()
         self.product_id = os.environ.get("TENNISVISION_PPIO_PRODUCT_ID", "L40S.22c125g")
         self.cluster_id = os.environ.get("TENNISVISION_PPIO_CLUSTER_ID", "cn-south-1")
